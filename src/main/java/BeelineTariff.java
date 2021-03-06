9e88ec5561df5ec319eb2109bfbbe224c541a4e3
Anvarmag <Anvarfon@gmail.com>
2021-03-06 20:34:41 +0300

    fixed #1

--- src/main/java/BeelineTariff.java
+++ src/main/java/BeelineTariff.java
@@ -1,9 +1,12 @@
-public class BeelineTariffs {
+public class BeelineTariff {
     long id;
     String name;
+    boolean eSim;
+    String url;
     int downloadGb;
     int minutes;
     int sms;
     int pricemonth;
     int oldpricemonth;
+    String discount;
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        BeelineTariffs tariff1 = new BeelineTariffs();
+        BeelineTariff tariff1 = new BeelineTariff();
         tariff1.id = 1;
         tariff1.name = "Близкие люди 1";
         tariff1.downloadGb = 15;
         tariff1.minutes = 400;
         tariff1.sms = 300;
         tariff1.pricemonth = 350;
+        System.out.println(tariff1.eSim);
         System.out.println(tariff1.name);
         System.out.println(tariff1.downloadGb);
         System.out.println(tariff1.minutes);
         System.out.println(tariff1.sms);
         System.out.println(tariff1.pricemonth);
+        System.out.println(tariff1.url);
 
-        BeelineTariffs tariff2 = new BeelineTariffs();
+        BeelineTariff tariff2 = new BeelineTariff();
         tariff2.id = 2;
         tariff2.name = "Близкие люди 2";
         tariff2.downloadGb = 30;
@@ -21,11 +23,15 @@ public class Main {
         tariff2.sms = 300;
         tariff2.oldpricemonth = 400;
         tariff2.pricemonth = 320;
+        tariff2.discount = "-20% на год";
+        System.out.println(tariff2.eSim);
         System.out.println(tariff2.name);
         System.out.println(tariff2.downloadGb);
         System.out.println(tariff2.minutes);
         System.out.println(tariff2.sms);
         System.out.println(tariff2.oldpricemonth);
+        System.out.println(tariff2.discount);
         System.out.println(tariff2.pricemonth);
+        System.out.println(tariff2.url);
     }
 }
